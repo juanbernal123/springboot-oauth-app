4f603017ade2abba7eb7c3e2ae0517d565935285
Juan Bernal <juan.bernal@itti.digital>
2025-03-26 11:24:31 -0300

    first commit

---
+++ src/test/java/com/juan/springboot/security/authserver/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.springboot.security.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
